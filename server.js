a55219af661ab44e4c199c6c02ac4eac3bbe2c0b
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-25 11:45:53 +0000

    Uncommenting GET route

--- server.js
+++ server.js
@@ -15,14 +15,13 @@ server.set('view engine', 'html');
 // Accessing CSS & JS files
 server.use(cors());
 server.use(express.static(`${__dirname}/public`));
-
 server.use(express.urlencoded({extended: true}));
 
 // Parses bodies from url
 server.use(bodyParser.urlencoded({extended: true}));
 
 // By entering localhost:8080 in the address bar, we navigate to another page
-// server.get('/', (req, res) => res.status(200).render('index'));
+server.get('/', (req, res) => res.status(200).render('index'));
 
 // Listening to the server at port 8080
 server.listen(8080, () => console.log('Listening to port 8080'));
