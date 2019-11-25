aea26fa0d713c4193630eb543001b7fc6dbc0723
Elizabeth luong <elizabethluong@live.co.uk>
2019-11-25 15:13:14 +0000

    add user and title inputs

--- server.js
+++ server.js
@@ -1,41 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-const http = require('http');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const http = require("http");
 
 // Making an express application
 const server = express();
 
-// Render HTML files (from 'views' directory) using the EJS views engine 
-server.set('views', `${__dirname}/views`);
-server.engine('html', require('ejs').renderFile);
-server.set('view engine', 'html');
+// Render HTML files (from 'views' directory) using the EJS views engine
+server.set("views", `${__dirname}/views`);
+server.engine("html", require("ejs").renderFile);
+server.set("view engine", "html");
 
 // Accessing CSS & JS files
 server.use(cors());
 server.use(express.static(`${__dirname}/public`));
-server.use(express.urlencoded({extended: true}));
+server.use(express.urlencoded({ extended: true }));
 
 // Parses bodies from url
-server.use(bodyParser.urlencoded({extended: true}));
-
+server.use(bodyParser.urlencoded({ extended: true }));
 
 // By entering localhost:8080 in the address bar, we navigate to another page
 
-server.get('/', (req, res) => res.status(200).render('index'));
+server.get("/", (req, res) => res.status(200).render("index.html"));
 
-server.get('/addPost', (req, res) => {
-    let newEntry = res.body.newEntry;
-    // res.status(200).render('addpost');
-    console.log(newEntry);
-      let title = req.body.title;
-    console.log(title)
-
-    let user = req.body.user;
-    console.log(user)
+server.get("/addPost", (req, res) => {
+  res.status(200).render("addpost");
 });
 
+server.post("/addPost", (req, res) => {
+  //   res.status(200).render("addpost");
+  let newEntry = req.body.newEntry;
+  console.log(newEntry);
+  let title = req.body.title;
+  console.log(title);
+  let user = req.body.user;
+  console.log(user);
+});
 
 // Listening to the server at port 8080
-server.listen(8080, () => console.log('Listening to port 8080'));
+server.listen(8080, () => console.log("Listening to port 8080"));
