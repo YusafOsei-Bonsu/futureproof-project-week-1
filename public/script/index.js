0821a40367ffa817e201cb0b55398abea0786213
Elizabeth luong <elizabethluong@live.co.uk>
2019-11-26 08:49:41 +0000

    change to ES6

--- public/script/index.js
+++ public/script/index.js
@@ -1,20 +1,18 @@
 function getData() {
-  var input = $("#giphy").val();
+  const input = $("#giphy").val();
   console.log(input);
 
-  var xhr = $.get(
-    "http://api.giphy.com/v1/gifs/search?q=" +
-      input +
-      "&api_key=rUL04f4RvzYBVhJdj5UbinsbiL0Bj2qd&limit=10"
+  const xhr = $.get(
+    `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=rUL04f4RvzYBVhJdj5UbinsbiL0Bj2qd&limit=10`
   );
-  xhr.done(function(response) {
+  xhr.done(response => {
     console.log("success got data", response);
 
     let gifs = response.data;
 
     for (i in gifs) {
       $(".giphycontainer").append(
-        "<img src='" + gifs[i].images.original.url + "' style= height: 100px'/>"
+        `<img src='${gifs[i].images.original.url}' style= height: 100px'/>`
       );
     }
   });
