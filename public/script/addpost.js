0a58fda920d7bf0b6ad6be79a6e66e9171716e37
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-26 14:59:03 +0000

    Wrote comments

--- public/script/addpost.js
+++ public/script/addpost.js
@@ -1,18 +1,17 @@
 $(document).ready(() => {
     
     $("#searchgiphy").on("click", () => {
-        const input = $("#giphy").val();
-        console.log(input);
+        const searchedGifTerm = $("#giphy").val();
 
-        const xhr = $.get(
-            `http://api.giphy.com/v1/gifs/search?q=${input}&api_key=rUL04f4RvzYBVhJdj5UbinsbiL0Bj2qd&limit=10`
-        );
-        
+        // Sending GET request to recieve Gifs, which includes searched term
+        const xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${searchedGifTerm}&api_key=rUL04f4RvzYBVhJdj5UbinsbiL0Bj2qd&limit=10`);
+
         xhr.done(response => {
             console.log("success got data", response);
 
             let gifs = response.data;
-
+            
+            // Display 10 gifs on the page
             for (let gif of gifs) {
                 $(".giphycontainer").append(
                     `<img src='${gif.images.original.url}' style= height: 100px'/>`
