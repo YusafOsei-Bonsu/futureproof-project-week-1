b8ceab0f5c18290c79c7db6838c252baccea4e34
Yusaf Osei-Bonsu <yusaf.osei-bonsu@kcl.ac.uk>
2019-11-28 17:04:41 +0000

    New route for the comment functionality

--- server.js
+++ server.js
@@ -17,35 +17,80 @@ server.set("view engine", "ejs");
 // Accessing CSS & JS files
 server.use(cors());
 server.use(express.static(`${__dirname}/public`));
-server.use(express.urlencoded({ extended: true }));
+server.use(express.urlencoded({
+  extended: true
+}));
 
 // Parses bodies from url
-server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 // By entering localhost:8080 in the address bar, we navigate to homepage
 server.get("/", (req, res) => {
   fs.readFile("./blog.json", "utf-8", (err, data) => {
     let blogEntries = JSON.parse(data);
-    res.status(200).render("index", { entries: blogEntries.entries });
-    console.log(JSON.parse(data))
+    res.status(200).render("index", {
+      entries: blogEntries.entries
+    });
   });
 });
 
-// Navigates to comments page. we must read the json file and parse the data into an object, then we are able to define blogIds as an array of the ids in the json file. we can then implement the logic to verify that the url extension matches an id in the blogIds array
-server.get("/blogPage/:id", (req, res) => {
-  const id = req.params.id;  
+// goes to the comments page
+server.get("/commentsPage", (req, res) => {
   fs.readFile("./blog.json", "utf-8", (err, data) => {
-    let blogs = JSON.parse(data);
-    let blogIds = Object.keys(blogs.entries);
+    let blogEntries = JSON.parse(data);
+    let entryID = req.query.entryID;
+    console.log(entryID);
+    console.log(blogEntries.entries[entryID]);
+    res.status(200).render("comments", {
+      entry: blogEntries.entries[entryID],
+      id: entryID
+    });
+  });
+});
+
+// Updates the comment of the respective entry
+server.post('/addComment', (req, res) => {
+  // Blog post ID
+  let entryID = req.body["entryID"];
+
+  //passing data from input into the json file
+  fs.readFile("./blog.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    console.log(req.body);
     
-  if (blogIds.includes(id) === true) {
-    console.log("yeehaw");
-    res.status(200).render("blogpage")
-  } else {
-    res.status(404).send(`Blog ${id} not found`)}
-  }
-  )})
-  
+    // The comment made by the user
+    let comment = req.body["comment"];
+    
+    let listOfEntries = JSON.parse(data);
+
+    listOfEntries.entries[entryID].comments.push(comment);
+
+    // Storing the entry in the json file
+    fs.writeFile("./blog.json", JSON.stringify(listOfEntries), "utf-8", err => {
+      if (err) throw err;
+    });
+  });
+
+  res.redirect(`http://localhost:8080/commentsPage?entryID=${entryID}`);
+});
+
+// Navigates to comments page. we must read the json file and parse the data into an object, then we are able to define blogIds as an array of the ids in the json file. we can then implement the logic to verify that the url extension matches an id in the blogIds array
+// server.get("/commentsPage/:id", (req, res) => {
+//   const id = req.params.id;  
+//   fs.readFile("./blog.json", "utf-8", (err, data) => {
+//     let blogs = JSON.parse(data);
+//     let blogIds = Object.keys(blogs.entries);
+
+//     if (blogIds.includes(id) === true) {
+//       // console.log("yeehaw");
+//       res.status(200).render("comments");
+//     } else {
+//       res.status(404).send(`Blog ${id} not found`)
+//     }
+//   });
+// });
 
 // Navigates to a page where users can create blog entries
 server.get("/postPage", (req, res) => res.status(200).render("addpost"));
@@ -56,15 +101,27 @@ server.post("/addPost", (req, res) => {
   fs.readFile("./blog.json", "utf-8", (err, data) => {
     if (err) throw err;
     let listOfEntries = JSON.parse(data);
-    // We're adding the user's entry to the list of entries
-    listOfEntries.entries[generateID()] = {title: req.body.title, author: req.body.user, entry: req.body.newEntry, reactions: {like: 0, dislike: 0, love: 0, happy: 0, sad: 0}};
-    // Storing the entry in the json file
 
+    // We're adding the user's entry to the list of entries
+    listOfEntries.entries[generateID()] = {
+      title: req.body.title,
+      author: req.body.user,
+      entry: req.body.newEntry,
+      reactions: {
+        like: 0,
+        dislike: 0,
+        love: 0,
+        happy: 0,
+        sad: 0
+      },
+      comments: []
+    };
+
+    // Storing the entry in the json file
     fs.writeFile("./blog.json", JSON.stringify(listOfEntries), "utf-8", err => {
-        if (err) throw err;
-        console.log("Done!");
-      }
-    );
+      if (err) throw err;
+      console.log("Done!");
+    });
   });
   //  Navigating back to the add-post page
   res.redirect("http://localhost:8080/postPage");
@@ -72,43 +129,41 @@ server.post("/addPost", (req, res) => {
 
 // To change emoji reaction values in .json file
 server.post('/emoji', (req, res) => {
-    //passing data from input into the json file
-    fs.readFile("./blog.json", "utf-8", (err, data) => {
+  //passing data from input into the json file
+  fs.readFile("./blog.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    console.log(req.body);
+    // The emoji selected by the user
+    let selectedEmoji = req.body["emoji"];
+    // ID of the blog post
+    let entryID = req.body["entryID"];
+    let listOfEntries = JSON.parse(data);
+
+    // We're adding the user's entry to the list of entries
+    switch (selectedEmoji) {
+      case "like":
+        listOfEntries.entries[entryID].reactions["like"] += 1;
+        break;
+      case "dislike":
+        listOfEntries.entries[entryID].reactions["dislike"] += 1;
+        break;
+      case "love":
+        listOfEntries.entries[entryID].reactions["love"] += 1;
+        break;
+      case "happy":
+        listOfEntries.entries[entryID].reactions["happy"] += 1;
+        break;
+      case "sad":
+        listOfEntries.entries[entryID].reactions["sad"] += 1;
+        break;
+    }
+
+    // Storing the entry in the json file
+    fs.writeFile("./blog.json", JSON.stringify(listOfEntries), "utf-8", err => {
       if (err) throw err;
-      console.log(req.body);
-      // The emoji selected by the user
-      let selectedEmoji = req.body["emoji"];
-      // ID of the blog post
-      let entryID = req.body["entryID"];
-      let listOfEntries = JSON.parse(data);
-
-      // We're adding the user's entry to the list of entries
-      switch (selectedEmoji) {
-        case "like":
-          listOfEntries.entries[entryID].reactions["like"] += 1; 
-          break;
-        case "dislike":
-          listOfEntries.entries[entryID].reactions["dislike"] += 1;
-          break;
-        case "love":
-          listOfEntries.entries[entryID].reactions["love"] += 1;
-          break;
-        case "happy":
-          listOfEntries.entries[entryID].reactions["happy"] += 1;
-          break;
-        case "sad":
-          listOfEntries.entries[entryID].reactions["sad"] += 1;
-          break;
-      }
-
-      // Storing the entry in the json file
-      fs.writeFile("./blog.json", JSON.stringify(listOfEntries), "utf-8", err => {
-          if (err) throw err;
-          console.log("Done!");
-        }
-      );
     });
-  
+  });
+
   // Update emoji values on homepage
   res.redirect('http://localhost:8080/');
 });
@@ -116,7 +171,7 @@ server.post('/emoji', (req, res) => {
 // Creates a 7-digit unique ID for blog entry
 const generateID = () => {
   let ID = "";
-  
+
   // Appends a single random character to ID
   for (let i = 1; i <= 7; i++) ID += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1)) + 65);
 
